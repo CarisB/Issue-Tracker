5275764fa47b3f6c89edb42dc90a9b9a4fdfc808
CarisB <chkbaek@gmail.com>
2024-06-22 13:58:14 -0500

    Added Issues list

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,38 @@
 "use client";
 
+import prisma from "@/prisma/db";
 import { Component1Icon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
 
-function IssuesPage() {
+async function IssuesPage() {
   const router = useRouter();
+  const issues = await prisma.issue.findMany();
 
   return (
     <div>
+      <Table.Root variant="surface" className="mb-10">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created On
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>{issue.title}</Table.Cell>
+              <Table.Cell>{issue.status}</Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toLocaleString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
       <Button
         variant="soft"
         className="cursor-pointer"
