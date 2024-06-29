2abe07a0d244a9759a5254a0de81bd7f7d8c51f7
CarisB <chkbaek@gmail.com>
2024-06-29 13:05:45 -0500

    Added PrismaAdapter to Auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,12 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  adapter: PrismaAdapter(new PrismaClient()),
+  session: {
+    strategy: "jwt",
+  },
 });
