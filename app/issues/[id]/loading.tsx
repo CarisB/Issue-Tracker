60b6c81692cc4441b04565efa4426747281e155b
CarisB <chkbaek@gmail.com>
2024-06-28 23:31:27 -0500

    Added loading page for [id]

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Loader from "@/app/_components/Loader";
+
+function LoadingIssuesDetailPage() {
+  return (
+    <div className="flex h-96 items-center justify-center">
+      <Loader />
+    </div>
+  );
+}
+
+export default LoadingIssuesDetailPage;
