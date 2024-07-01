203c1f4c3650f5c9e4874889a10a8fd80a4b0bc6
CarisB <chkbaek@gmail.com>
2024-06-30 22:59:58 -0500

    Refactored NavMenu into separate (embedded) component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,14 +7,7 @@ import { usePathname } from "next/navigation";
 import { BsBugFill } from "react-icons/bs";
 import UserMenu from "./UserMenu";
 
-function NavBar() {
-  const links = [
-    { href: "/", label: "Dashboard" },
-    { href: "/issues", label: "Issues" },
-  ];
-
-  const currentPath = usePathname();
-
+export default function NavBar() {
   return (
     <nav className="p-5 bg-teal-950 text-white">
       <Container>
@@ -23,21 +16,7 @@ function NavBar() {
             <Link href="/">
               <BsBugFill size={30} />
             </Link>
-            <ul className="flex space-x-6 p-5 bg-teal-900 rounded-xl">
-              {links.map(({ href, label }) => (
-                <li key={href}>
-                  <Link
-                    href={href}
-                    className={classNames({
-                      "font-bold": currentPath === href,
-                      "hover:text-emerald-200 transition-colors": true,
-                    })}
-                  >
-                    {label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavMenu />
           </Flex>
           <Flex align="center">
             <UserMenu />
@@ -48,4 +27,29 @@ function NavBar() {
   );
 }
 
-export default NavBar;
+function NavMenu() {
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+  ];
+
+  const currentPath = usePathname();
+
+  return (
+    <ul className="flex space-x-6 p-5 bg-teal-900 rounded-xl">
+      {links.map(({ href, label }) => (
+        <li key={href}>
+          <Link
+            href={href}
+            className={classNames({
+              "font-bold": currentPath === href,
+              "hover:text-emerald-200 transition-colors": true,
+            })}
+          >
+            {label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
