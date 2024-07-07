ec5128354d3a29372c727fdc7e7d576fd6e27be1
CarisB <chkbaek@gmail.com>
2024-07-06 19:40:35 -0500

    Fixed Dark mode toggle bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <AuthProvider>
           <SessionProvider>
             <QueryClientProvider>
-              <ThemeProvider attribute="class">
+              <ThemeProvider attribute="class" enableSystem={false}>
                 <Theme accentColor="cyan">
                   <NavBar />
                   <Container>
